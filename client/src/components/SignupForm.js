6e853ef49399abe7ba098fcc31137b4b10bfb74e
drad <robertjdstoddard@gmail.com>
2021-09-30 14:50:51 -0600

    login and signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import {useMutation} from '../utils/auth'
+import {ADD_USER} from '../utils/mutations'
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +11,18 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, {error}] = useMutation(ADD_USER)
+  useEffect(() =>{
+    if(error){
+      setShowAlert(true)
+    }
+    else{
+      setShowAlert(false)
+    }
+  },
+  [error]
+  )
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -21,25 +33,15 @@ const SignupForm = () => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: {...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+      setShowAlert(true)
     }
 
     setUserFormData({
