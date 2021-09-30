d36d52684157028d4a6d3de564f17df6bd9d06ba
drad <robertjdstoddard@gmail.com>
2021-09-30 14:20:18 -0600

    queries and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import {gql} from '@apollo/client'
+
+export const GET_ME = gql `
+{
+    me{
+        _id
+        username
+        email
+        bookCount
+        saveBooks{
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
+}`
