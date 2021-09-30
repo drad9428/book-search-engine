f7ab73dde5d714e15efed79d0ab06f01e026a046
drad <robertjdstoddard@gmail.com>
2021-09-30 13:21:02 -0600

    server and auth plus schema start

--- server/server.js
+++ server/server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const {ApolloServer} = require('apollo-server-express')
+const {typeDefs, resolvers} = require('./schemas')
+const {authMiddleware} = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+async function startServer(){
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  })
+  await server.start()
+  server.applyMiddleware({app})
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,3 +32,5 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startServer()
